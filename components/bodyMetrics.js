2eefebfcee8d905f6405e9df8b388c4089bf7ed7
FraustoB <brandon.frausto18@gmail.com>
2021-10-15 22:01:28 -0500

    added basic grid layout future installments on features down the road. Fixed error with key elements by adding react fragments

--- components/bodyMetrics.js
+++ components/bodyMetrics.js
@@ -1,59 +1,52 @@
-
-import {useEffect ,useState} from 'react';
+import React, { useEffect, useState } from "react";
 import activityLvls from "../information/activityLvls";
-import styles from '/styles/bodymetrics.module.scss'
+import styles from "/styles/bodymetrics.module.scss";
 
-export default function BodyMetrics({props,calories,setCalories}){
+export default function BodyMetrics({ props, calories, setCalories }) {
+  const [curBmr, setCurBmr] = useState(0);
 
-    const [curBmr, setCurBmr ] = useState(0);
+  let { sex, age, height, weight, activityLvl } = props;
 
-    let {sex,age,height,weight,activityLvl} = props;
+  useEffect(() => {
+    console.log(props);
+    setCurBmr(bmrCalc(weight, height, sex, age));
+  }, [props]);
+  console.log(curBmr);
 
-    useEffect(()=> {
-        console.log(props);
-        setCurBmr(bmrCalc(weight,height,sex,age)); 
-    },[props])
-      console.log(curBmr)
-
-
-    function bmrCalc(weight,height,sex,age){
-        //first convert LB weight into kg's
-        let weightKg = weight/2.2;
-        // initialize a user bmr for later
-        let userBmr = '';
-        //determine if user is a male or female, then use the appropriate calculations
-        //to figure out their bmr
-        if(sex==='male') {
-            userBmr = (10*weightKg) + (6.25 * height) -(5*age)+5
-        } 
-        else {
-            userBmr = (10*weightKg) +  (6.25 * height) - (5*age)-161;
-        }
-        userBmr = Math.floor(userBmr);
-        return userBmr;
+  function bmrCalc(weight, height, sex, age) {
+    //first convert LB weight into kg's
+    let weightKg = weight / 2.2;
+    // initialize a user bmr for later
+    let userBmr = "";
+    //determine if user is a male or female, then use the appropriate calculations
+    //to figure out their bmr
+    if (sex === "male") {
+      userBmr = 10 * weightKg + 6.25 * height - 5 * age + 5;
+    } else {
+      userBmr = 10 * weightKg + 6.25 * height - 5 * age - 161;
     }
+    userBmr = Math.floor(userBmr);
+    return userBmr;
+  }
 
-    const activityCalories =  activityLvls.map((cur) => (
-        <>
-           <div key={cur.value}>{cur.activity}</div><div>{Math.floor(cur.value * curBmr)} calories per day </div>
-        </>
-        
+  const activityCalories = activityLvls.map((cur) => (
+    //need to use React.Fragments in order to avoid errors in react but also not introduce
+    //new elements to the dom
+    <React.Fragment key={cur.activity}>
+      <div key={cur.value}>{cur.activity}</div>
+      <div key={cur.activity}>
+        {Math.floor(cur.value * curBmr)} calories per day
+      </div>
+    </React.Fragment>
+  ));
 
-    ))
-         
-    
-
-
-        
-    return(
-        <div className={styles.calorieSection}>
-            <div className={styles.mntncCal} >
-            <div className={styles.actvForm}>
-                {activityCalories}  
-            </div>
-            
-
-        </div>
-        </div>
-    )
+  return (
+    <div className={styles.calorieSection}>
+      <div className={styles.mntncCal}>
+        <div className={styles.calPerDay}>Something here</div>
+        <div>Something Here</div>
+      </div>
+      <div className={styles.actvForm}>{activityCalories}</div>
+    </div>
+  );
 }
--- components/forms.js
+++ components/forms.js
@@ -1,93 +1,123 @@
-import {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../styles/form.module.scss';
-import heights from '../information/height';
-import activityLvls from '../information/activityLvls';
-import {useRouter} from 'next/router';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../styles/form.module.scss";
+import heights from "../information/height";
+import activityLvls from "../information/activityLvls";
+import { useRouter } from "next/router";
 
-export default function TdeeForm(){
-       const router = useRouter();
-        const [params, setParams] = useState({
-            age:'',
-            height: '',
-            weight: '',
-            activityLvl: '',
-            sex:''
-        })
-        // Destructer state to allow cleaner data
-        const {age,height,weight,activityLvl,sex} = params;
+export default function TdeeForm() {
+  const router = useRouter();
+  const [params, setParams] = useState({
+    age: "",
+    height: "",
+    weight: "",
+    activityLvl: "",
+    sex: "",
+  });
+  // Destructer state to allow cleaner data
+  const { age, height, weight, activityLvl, sex } = params;
 
-        //This function handles basic form input to allow for state-change
-        const handleChange=(e)=>setParams({...params, [e.target.name]: e.target.value});
+  //This function handles basic form input to allow for state-change
+  const handleChange = (e) =>
+    setParams({ ...params, [e.target.name]: e.target.value });
 
-        //Pushes Data inputted by user to the page that will calculate TDEE
-        const handleSubmit =(e) =>{
-            e.preventDefault();
-            console.log('submit', params)
-            router.push(`/tdeeresults/data?sex=${sex}&age=${age}&height=${height}&weight=${weight}&activityLvl=${activityLvl}`);
-        }
+  //Pushes Data inputted by user to the page that will calculate TDEE
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit", params);
+    router.push(
+      `/tdeeresults/data?sex=${sex}&age=${age}&height=${height}&weight=${weight}&activityLvl=${activityLvl}`
+    );
+  };
 
-        // List of heights in inches and centimeters used by the dropdown menu
-        const optionItems = heights.map(height =>
-            <option key={height.height} value={height.value}>{height.height}</option>);
+  // List of heights in inches and centimeters used by the dropdown menu
+  const optionItems = heights.map((height) => (
+    <option key={height.height} value={height.value}>
+      {height.height}
+    </option>
+  ));
 
-        // imported activity lvls to also map over and use during for dropdown menu
-        const optionActivity = activityLvls.map(actvty => 
-            <option key={actvty.value} value={actvty.value}>{actvty.activity}{actvty.description}</option>);
+  // imported activity lvls to also map over and use during for dropdown menu
+  const optionActivity = activityLvls.map((actvty) => (
+    <option key={actvty.value} value={actvty.value}>
+      {actvty.activity}
+      {actvty.description}
+    </option>
+  ));
 
-      
+  return (
+    <>
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit}>
+          <section>
+            <input
+              type="radio"
+              checked={sex === "male"}
+              id="sexMale"
+              value="male"
+              name="sex"
+              onChange={handleChange}
+            />
+            <label htmlFor="sexMale">Male</label>
 
-    return(
-        <>
-        <div className={styles.form}>
-            <form onSubmit={handleSubmit}>
-
-            <section> 
-                <input type='radio' checked={sex === 'male'} id='sexMale' value='male' name='sex' onChange={handleChange}/>
-                <label htmlFor='sexMale'>Male</label>
-               
-               
-                <input type='radio' checked={sex ==='female'}id ='sexFemale' value='female' name='sex' onChange={handleChange}/>
-                <label htmlFor='sexFemale'>Female</label>
-
-            </section>
-            <section>
+            <input
+              type="radio"
+              checked={sex === "female"}
+              id="sexFemale"
+              value="female"
+              name="sex"
+              onChange={handleChange}
+            />
+            <label htmlFor="sexFemale">Female</label>
+          </section>
+          <section>
             <label className={styles.label}>Age</label>
-            <input onChange={handleChange} className={styles.input} name='age'value={age}></input>
-            </section>
+            <input
+              onChange={handleChange}
+              className={styles.input}
+              name="age"
+              value={age}
+            ></input>
+          </section>
 
-            <br/>
+          <br />
 
-            <div>
+          <div>
             <label className={styles.label}>Weight</label>
-            <input className={styles.input} onChange={handleChange}name='weight' value={weight}></input>
-            </div>
+            <input
+              className={styles.input}
+              onChange={handleChange}
+              name="weight"
+              value={weight}
+            ></input>
+          </div>
 
-            <br/>
+          <br />
 
-            <div>
+          <div>
             <label className={styles.label}>Height</label>
-            <select name='height'value={height} onChange={handleChange}>
-                {optionItems}
+            <select name="height" value={height} onChange={handleChange}>
+              {optionItems}
             </select>
-            </div>
-          
+          </div>
 
-            <br/>
+          <br />
 
-            <div>
-            <label  className={styles.label}>Activity</label>
-            <select onChange={handleChange} name='activityLvl' className={styles.input}>
-                {optionActivity}
+          <div>
+            <label className={styles.label}>Activity</label>
+            <select
+              onChange={handleChange}
+              name="activityLvl"
+              className={styles.input}
+            >
+              {optionActivity}
             </select>
-            </div>
+          </div>
 
-            <br/>
-            <button>Calculate</button>
-            </form>
-        </div>
-
-    
-        </>
-    )
+          <br />
+          <button>Calculate</button>
+        </form>
+      </div>
+    </>
+  );
 }
