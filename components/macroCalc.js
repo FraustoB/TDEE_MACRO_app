ac824d1ed22d0faaaec88e1796c2f101caf9fb69
FraustoB <brandon.frausto18@gmail.com>
2021-11-28 19:15:42 -0600

    added styles for varying carb amounts

--- components/macroCalc.js
+++ components/macroCalc.js
@@ -1,19 +1,82 @@
 import styles from "/styles/macrocalc.module.scss";
-import { calculateModCarbMacros } from "../functions/calculateMacros";
+import {
+  calculateModCarbMacros,
+  calculateLowCarbMacros,
+  calculateHighCarbMacros,
+} from "../functions/calculateMacros";
 
-import calculateMacros from "../functions/calculateMacros";
+import { useState } from "react";
 export default function MacroEditor({ caloriesToCalc }) {
+  const [clicked, setClicked] = useState({
+    isMntncClicked: true,
+    isCuttingclicked: false,
+    isBulkingClicked: false,
+  });
+
   const cuttingCalories = caloriesToCalc - 500;
   const bulkingCalories = caloriesToCalc + 500;
   const maintenanceCalories = caloriesToCalc;
+  const modCarbMacros = calculateModCarbMacros(maintenanceCalories);
+  const lowCarbMacros = calculateLowCarbMacros(maintenanceCalories);
+  const highCarbMacros = calculateHighCarbMacros(maintenanceCalories);
 
-  const macros = calculateModCarbMacros(maintenanceCalories);
-  console.log(macros, "from macroCalc.js");
+  console.log(
+    modCarbMacros,
+    lowCarbMacros,
+    highCarbMacros,
+    "from macroCalc.js"
+  );
 
   return (
-    <div>
-      These Macronutrient alues reflect your maintenance claories of{" "}
-      {caloriesToCalc} per day
+    <div className={styles.macros}>
+      <span className={styles.macroCalories}>
+        These Macronutrient Values reflect your maintenance calories of{" "}
+        <span className={styles.dailyCal}>{caloriesToCalc}</span> calories per
+        day
+      </span>
+
+      <div className={styles.macroGrid}>
+        <div className={styles.modCarbs}>
+          <span className={styles.macroTitle}>Moderate Carbs</span>
+          <br />
+          <h2> {modCarbMacros.protein}g</h2>
+          <p>protein</p>
+          <br />
+          <h2> {modCarbMacros.carbs}g</h2>
+          <p>carbohydrates</p>
+          <br />
+          <h2> {modCarbMacros.fats}g</h2>
+          <p>fats</p>
+          <br />
+        </div>
+
+        <div className={styles.lowCarbs}>
+          <span className={styles.macroTitle}>low Carbs</span>
+          <br />
+          <h2> {lowCarbMacros.protein}g</h2>
+          <p>protein</p>
+          <br />
+          <h2> {lowCarbMacros.carbs}g</h2>
+          <p>carbohydrates</p>
+          <br />
+          <h2> {lowCarbMacros.fats}g</h2>
+          <p>fats</p>
+          <br />
+        </div>
+        <div className={styles.highCarbs}>
+          <span className={styles.macroTitle}>High Carbs</span>
+          <br />
+          <h2> {highCarbMacros.protein}g</h2>
+          <p>protein</p>
+          <br />
+          <h2> {highCarbMacros.carbs}g</h2>
+          <p>carbohydrates</p>
+          <br />
+          <h2> {highCarbMacros.fats}g</h2>
+          <p>fats</p>
+          <br />
+        </div>
+      </div>
     </div>
   );
 }
--- functions/calculateMacros.js
+++ functions/calculateMacros.js
@@ -15,12 +15,25 @@ function calculateLowCarbMacros(calories) {
   let protein = 0;
   let fats = 0;
   let carbs = 0;
+
+  protein = Math.floor((calories * 0.4) / 4);
+  fats = Math.floor((calories * 0.4) / 9);
+  carbs = Math.floor((calories * 0.2) / 4);
+  return { protein, carbs, fats };
 }
 
 function calculateHighCarbMacros(calories) {
   let protein = 0;
   let fats = 0;
   let carbs = 0;
+  protein = Math.floor((calories * 0.3) / 4);
+  fats = Math.floor((calories * 0.2) / 9);
+  carbs = Math.floor((calories * 0.5) / 4);
+  return { protein, carbs, fats };
 }
 
-export { calculateModCarbMacros };
+export {
+  calculateModCarbMacros,
+  calculateHighCarbMacros,
+  calculateLowCarbMacros,
+};
